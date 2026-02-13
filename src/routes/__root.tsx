4358ebd17c5794c5ef5efe0a3f73d9f2f0be1b5a
raulshma <raulshma@gmail.com>
2026-02-13 20:39:42 +0530

    refactor: remove unused font preconnect and stylesheet links

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -39,19 +39,6 @@ export const Route = createRootRoute({
         rel: 'stylesheet',
         href: appCss,
       },
-      {
-        rel: 'preconnect',
-        href: 'https://fonts.googleapis.com',
-      },
-      {
-        rel: 'preconnect',
-        href: 'https://fonts.gstatic.com',
-        crossOrigin: 'anonymous',
-      },
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
-      },
     ],
   }),
 
